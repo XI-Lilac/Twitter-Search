4086b9c2ad47d056abf2bb9be71b7da1dea4558f
XI-Lilac <xi.lilac@gmail.com>
2019-03-15 08:11:17 +0900

    bundle update

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,17 +1,10 @@
 class TwitterController < ApplicationController
   def search
     @tw_result = TwSearchResult.new
-    if @tw_result && params[:word]
+    if params[:word]
       @tw_result.word = params[:word]
-      @tw_result.tweet = @tw_result.client.search("-RT #{@tw_result.word} ", lang: "ja", result_type: 'recent', count: params[:limit])
-      respond_to do |format|
-      	format.html
-      end
+      @tw_result.tweet = @tw_result.client.search("-RT #{params[:word]} ", result_type: 'recent', count: 10)
+
     end
-    
-    rescue => e
-    logger.error e.message
-    flash[:error] = "エラーが起きました[#{e.message}]"
-    render root_path
-    end
+  end
 end
